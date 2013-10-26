d0634698cf8666fec2b4ab3a7d6bd2ef423db4cb
Rodrigo Dlugokenski <rddweb@gmail.com>
2013-10-26 17:25:12 -0200

    Acho que vou construir um novo controller para o pacman extendendo o atual.

---
+++ Assets/Scripts/QPacman_Controller.cs
@@ -0,0 +1,15 @@
+/**
+ * Pacman com Q-Learning, Por Rodrigo Dlugokenski
+ */
+
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof (CharacterController))]
+
+public class QPacman_Controller : PacMan_Controller	{
+	
+}
+
